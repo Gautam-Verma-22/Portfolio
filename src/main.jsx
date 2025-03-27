583bf5b7e9a1e42a72dfbfcea0fc906e4d4690ea
Garv <vermag667@gmail.com>
2025-03-27 14:00:48 +0530

    analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./state/store.js";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>
 );
